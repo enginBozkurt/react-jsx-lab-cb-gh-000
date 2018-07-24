249a710916866fdfe407600a0bf20d7573e921e9
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-24 11:41:38 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 class FillerText extends React.Component {
   render() {
     return (
-      
-      
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
     )
   }
 }
